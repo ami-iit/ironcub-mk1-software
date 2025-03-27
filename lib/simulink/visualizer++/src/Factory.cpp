36e4586f59f64e39efcd16abfaf6b1af624be9ee
Antonello Paolino <75119799+antonellopaolino@users.noreply.github.com>
2025-03-26 18:11:09 -0700

    Fix compilation issue (#25)

--- lib/simulink/visualizer++/src/Factory.cpp
+++ lib/simulink/visualizer++/src/Factory.cpp
@@ -1,7 +1,7 @@
 #include <Visualizer/Visualizer.h>
 
 // Class factory API
-#include <shlibpp/SharedLibraryClassApi.h>
+#include <sharedlibpp/SharedLibraryClassApi.h>
 
 // Add the example::SignalMath class to the plugin factory
 SHLIBPP_DEFINE_SHARED_SUBCLASS(iDynTreeVisualizer, ironcub::Visualizer, blockfactory::core::Block);
